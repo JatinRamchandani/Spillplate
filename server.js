e623a1b0a9cd57a7af161205d62ced5190b929c7
Jatin Ramchandani <jatinramchandani307@gmail.com>
2021-11-24 19:53:52 +0530

    s3

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ io.on('connection', (socket)=>{
 })
 
 
+app.get("/",(req,res)=>{
+    res.sendFile(path.join(__dirname, 'spillplate/build', 'index.html'));
+})
+
+
 if(process.env.NODE_ENV == "production"){
     app.use(express.static("spillplate/build"))
     app.get('*', function(req, res) {
